4c8dc6a5373808be124a5745253ba3b4ba722895
KevinTitanZ <klordonez1@espe.edu.ec>
2026-02-12 22:32:37 -0500

    Add Swashbuckle.AspNetCore package

--- Demo.CICD.Api/obj/Debug/net10.0/Demo.CICD.Api.AssemblyInfo.cs
+++ Demo.CICD.Api/obj/Debug/net10.0/Demo.CICD.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.CICD.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c87159226e9630a57cda23ce190bad623f75474")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.CICD.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.CICD.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
